f26f559f82b17fb0495f2f0470e56c91ebbf9e27
gauravkhatik <gauravkkhatik07@gmail.com>
2025-09-26 17:33:31 +0530

    first add all file

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+export default {
+  providers: [
+    {
+  
+      domain: "https://kind-boxer-42.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+  
+    
+
+    
+    </div> 
+  );
+}
